5894daae39b52baec475f085a6ceb60ea4964783
azzzertyy <42524344+azzzertyy@users.noreply.github.com>
2022-10-03 09:18:27 +0100

    fixes odds
    the gambit was ever so slightly more common than it should be

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ struct Data {}
 /// Cmon, why wait? Gramma will decide your fate.
 #[poise::command(slash_command, prefix_command)]
 async fn gambit(ctx: Context<'_>) -> Result<(), Error> {
-    let x: i32 = rand::thread_rng().gen_range(1..1001);
+    let x: i32 = rand::thread_rng().gen_range(1..1003);
     let mut result = "not";
     match x {
         1..=2 => result = "https://cdn.discordapp.com/attachments/1026290034980167720/1026290159634886667/gramma_gamU.gif",
-        3..=501 => result = "https://cdn.discordapp.com/attachments/1026290034980167720/1026290160410841098/gramma_gam1.gif",
+        3..=502 => result = "https://cdn.discordapp.com/attachments/1026290034980167720/1026290160410841098/gramma_gam1.gif",
         _ => result = "https://cdn.discordapp.com/attachments/1026290034980167720/1026290160012369920/gramma_gam2.gif",
     };
     ctx.say(result).await?;
